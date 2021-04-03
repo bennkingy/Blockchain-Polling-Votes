d850af0afca7ffad20398f6cb9cfb59ac1ec8027
Benn King <bennkingy@gmail.com>
2021-04-03 16:58:25 +0100

    finished smart contract

--- src/app/solidity/poll.sol
+++ src/app/solidity/poll.sol
@@ -1,31 +1,27 @@
-pragma solidity ^0.8.1;
+pragma solidity ^0.6.6;
 
 contract PollContract {
 
-    // Poll structure
     struct Poll {
         uint256 id;
         string question;
-        string image;
+        string thumbnail;
         uint64[] votes;
         bytes32[] options;
     }
 
-    // Voter structure
     struct Voter {
         address id;
         uint256[] votedIds;
         mapping(uint256 => bool) votedMap;
     }
 
-    // Empty array to hold polls
     Poll[] private polls;
-
-    // Map wallet address to voter
     mapping(address => Voter) private voters;
 
-    // Create poll function
-    function createPoll(string memory _question, string memory _image, bytes32[] memory _options) public {
+    event PollCreated(uint256 _pollId);
+
+    function createPoll(string memory _question, string memory _thumb, bytes32[] memory  _options) public {
         require(bytes(_question).length > 0, "Empty question");
         require(_options.length > 1, "At least 2 options required");
 
@@ -34,12 +30,45 @@ contract PollContract {
         Poll memory newPoll = Poll({
             id: pollId,
             question: _question,
-            image: _image,
+            thumbnail: _thumb,
             options: _options,
             votes: new uint64[](_options.length)
         });
 
-        // Add new poll to empty Poll array
         polls.push(newPoll);
+        emit PollCreated(pollId);
+    }
+
+    function getPoll(uint256 _pollId) external view returns(uint256, string memory, string memory, uint64[] memory, bytes32[] memory) {
+        require(_pollId < polls.length && _pollId >= 0, "No poll found");
+        return (
+            polls[_pollId].id,
+            polls[_pollId].question,
+            polls[_pollId].thumbnail,
+            polls[_pollId].votes,
+            polls[_pollId].options
+        );
+    }
+
+    function vote(uint256 _pollId, uint64 _vote) external {
+        require(_pollId < polls.length, "Poll does not exist");
+        require(_vote < polls[_pollId].options.length, "Invalid vote");
+        require(voters[msg.sender].votedMap[_pollId] == false, "You already voted");
+
+        polls[_pollId].votes[_vote] +=1 ;
+
+        voters[msg.sender].votedIds.push(_pollId);
+        voters[msg.sender].votedMap[_pollId] = true;
+    }
+
+    function getVoter(address _id) external view returns(address, uint256[] memory) {
+        return (
+            voters[_id].id,
+            voters[_id].votedIds
+        );
+    }
+
+    function getTotalPolls() external view returns(uint256) {
+        return polls.length;
     }
 }
